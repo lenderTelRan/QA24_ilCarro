4f8ea4736f91a7f7770909327c9ab83ba21a43f3
lenderTelRan <lender.vladimir@gmail.com>
2024-03-15 19:48:26 +0200

    first commit

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,20 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.navigate().to("https://ilcarro.web.app/");
+    }
+    public void stop() {
+        wd.quit();
+    }
+}
